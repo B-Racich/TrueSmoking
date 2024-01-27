15b0a6a611583a9947a7dd63b1c29fef2442acc0
B-Racich <32187610+B-Racich@users.noreply.github.com>
2024-01-26 17:09:03 -0800

    Add check for mod

--- Contents/mods/TrueSmoking/media/lua/client/Patches/AutoSmoke/AutoSmokeOverrides.lua
+++ Contents/mods/TrueSmoking/media/lua/client/Patches/AutoSmoke/AutoSmokeOverrides.lua
@@ -1,18 +1,20 @@
-local originalCanSmoke = AutoSmoke.canSmoke
-function AutoSmoke:canSmoke()
-    return not getPlayer():getModData().isSmoking and originalCanSmoke(self)
-end
-
-local oldFunc = onKeyPressed
-Events.OnKeyPressed.Remove(oldFunc)
-
-local function onKeyPressed(key)
-    if AutoSmoke.player and key == AutoSmoke.Options.keySmoke.key and AutoSmoke:canSmoke() then
-        if AutoSmoke.Options.characterSpeaks then
-            AutoSmoke.pressedKey = true
-        end
-        AutoSmoke:checkInventory()
+if getActivatedMods():contains("AutoSmoke") then
+    local originalCanSmoke = AutoSmoke.canSmoke
+    function AutoSmoke:canSmoke()
+        return not getPlayer():getModData().isSmoking and originalCanSmoke(self)
     end
-end
 
-Events.OnKeyPressed.Add(onKeyPressed)
+    local oldFunc = onKeyPressed
+    Events.OnKeyPressed.Remove(oldFunc)
+
+    local function onKeyPressed(key)
+        if AutoSmoke.player and key == AutoSmoke.Options.keySmoke.key and AutoSmoke:canSmoke() then
+            if AutoSmoke.Options.characterSpeaks then
+                AutoSmoke.pressedKey = true
+            end
+            AutoSmoke:checkInventory()
+        end
+    end
+
+    Events.OnKeyPressed.Add(onKeyPressed)
+end
